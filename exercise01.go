5e1d36958e95e63cc3e995dfedff1dd04c191d24
rmweir <ricardo.weir1@gmail.com>
2018-12-16 14:16:31 -0700

    finished exercise 1

---
+++ exercise01.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Short declaration operator
+	x := 42
+	y := "James Bond"
+	z := true
+
+	// Print using single line
+	fmt.Println(x, y, z)
+
+	// Print using multiple lines
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
+
