8365512eb9f1e8065dec7ff22d8062cc607d09f2
rmweir <ricardo.weir1@gmail.com>
2018-12-23 23:42:57 -0700

    did ex 4

--- LevelSix/exercise04.go
+++ LevelSix/exercise04.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
-func main() {
+type person struct {
+	first string
+	last string
+	age int
+}
 
+func main() {
+	v := person {
+		first: "Dr",
+		last: "Seuss",
+		age: 88,
+	}
+	v.speak()
+}
+
+func (per person) speak() {
+	fmt.Println("Name:", per.first, per.last)
+	fmt.Println("Age:", per.age)
 }
