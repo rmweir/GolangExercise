74372b291da441f988ca6e8e9bf5bdd440d11f34
rmweir <ricardo.weir1@gmail.com>
2019-01-04 00:32:08 -0700

    finished level 10 and start level 11

---
+++ LevelEleven/exercise01.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"log"
+)
+
+type person struct {
+	First string
+	Last string
+	Sayings []string
+}
+
+func main() {
+	p1 := person {
+		First: "James",
+		Last: "Bond",
+		Sayings: []string{"Shaken, not stirred.", "Any last wishes?", "Never say never"},
+	}
+
+	bs, err := json.Marshal(p1)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(string(bs))
+}
